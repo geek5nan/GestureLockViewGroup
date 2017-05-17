c904574a88e539f23a1163ff425d0f60d323e413
WuNan <me@devwu.com>
2017-05-17 16:56:07 +0800

    - remove redundant data - update example code

--- app/src/main/java/com/devwu/gesturelockviewgroup/demo/GestureLockActivity.java
+++ app/src/main/java/com/devwu/gesturelockviewgroup/demo/GestureLockActivity.java
@@ -12,6 +12,7 @@ import com.devwu.common.utils.ToastUtil;
 import com.devwu.gesturelockviewgroup.GestureLockViewGroup;
 import com.devwu.gesturelockviewgroup.listener.SettingListener;
 import com.devwu.gesturelockviewgroup.listener.VerifyListener;
+import com.devwu.gesturelockviewgroup.provider.nodeview.NodeViewProviderImage;
 import com.devwu.gesturelockviewgroupdemo.R;
 
 import java.lang.annotation.Retention;
@@ -54,12 +55,13 @@ public class GestureLockActivity extends AppCompatActivity {
                         mGestureLockViewGroup.getPasswordProvider().removePassword();
                         mGestureLockViewGroup.resetView();
                         mPromptText.setText("请绘制新手势密码！");
+                        ToastUtil.showShort("此时点击返回键可关闭手势锁");
                     }else {
                         mPromptText.setText("手势密码正确");
                         ToastUtil.showShort("手势密码正确");
                         finish();
                         if (mState == STATE.VERIFY){
-
+                            //验证成功，此处可跳转至任意位置
                         }
                     }
                 } else {
@@ -103,6 +105,7 @@ public class GestureLockActivity extends AppCompatActivity {
                 }
             }
         });
+        mGestureLockViewGroup.setNodeViewProvider(new NodeViewProviderImage.Builder(this).build());
     }
 
     private void initView() {
@@ -126,7 +129,11 @@ public class GestureLockActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * 实际应用中需重写该方法，防止手势验证Activity被销毁
+     */
     @Override
     public void onBackPressed() {
+        super.onBackPressed();
     }
 }
